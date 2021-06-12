b98492a4d184df780a44b1ca0d255108f9303066
Anil Chouhan <76151927+anilchouhan8480@users.noreply.github.com>
2021-06-12 08:50:10 +0530

    Update settings.py

--- EcommerceProject/settings.py
+++ EcommerceProject/settings.py
@@ -140,4 +140,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'anilchouhan8480@gmail.com'
-EMAIL_HOST_PASSWORD = '0395@Anil'
+EMAIL_HOST_PASSWORD = '*********'
